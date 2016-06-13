7df53f7a1e750efb296538907ed36e589d446ee0
Brieuc <btribouillet@studio-goliath.com>
2016-06-13 09:41:39 +0200

    Plugins bricks using ACF

--- web/wp-content/plugins/bricks/bricks.php
+++ web/wp-content/plugins/bricks/bricks.php
@@ -7,9 +7,55 @@
  * Author URI: http://www.studio-goliath.com/
  */
 
-/**
- * Includes all the post-types
- */
-foreach(glob( plugin_dir_path( __FILE__ ) . '/post-types/*.php' ) as $filename) {
-    require_once $filename;
-}
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array (
+        'key' => 'group_575ad2a97bae8',
+        'title' => 'Bricks',
+        'fields' => array (
+            array (
+                'key' => 'field_575ad2e20ae06',
+                'label' => 'Brick',
+                'name' => 'brick',
+                'type' => 'flexible_content',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array (
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'button_label' => 'Add Row',
+                'min' => '',
+                'max' => '',
+                'layouts' => apply_filters( 'layouts_filter', array())
+            ),
+        ),
+        'location' => array (
+            array (
+                array (
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'post',
+                ),
+            ),
+            array (
+                array (
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'page',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => 1,
+        'description' => '',
+    ));
+
+endif;
